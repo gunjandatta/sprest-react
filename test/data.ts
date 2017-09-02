c27679175a1b856650372cd68b53ecb4feb31d71
Gunjan Datta <me@dattabase.com>
2017-09-01 23:14:19 -0400

    Removed the save method from the data file.

--- test/data.ts
+++ test/data.ts
@@ -5,7 +5,7 @@ import { IWebPartListCfg } from "../src";
 /**
  * Test Item Information
  */
-export interface ITestItem extends Types.IListItemResult {
+export interface ITestItem extends Types.IListItemQueryResult {
     Attachments?: boolean;
     TestBoolean?: boolean;
     TestChoice?: string;
@@ -64,7 +64,7 @@ export class DataSource {
      */
 
     // Method to load the test data
-    load = (itemId?: number): PromiseLike<ITestItem | Array<ITestItem>> => {
+    load = (): PromiseLike<Array<ITestItem>> => {
         // Return a promise
         return new Promise((resolve, reject) => {
             // Get the web
@@ -75,10 +75,8 @@ export class DataSource {
                 .Items()
                 // Set the query
                 .query({
-                    Filter: itemId > 0 ? "ID eq " + itemId : "",
-                    Expand: ["AttachmentFiles", "TestLookup", "TestMultiLookup", "TestMultiUser", "TestUser"],
                     OrderBy: ["Title"],
-                    Select: ["*", "Attachments", "AttachmentFiles", "TestLookup/ID", "TestLookup/Title", "TestMultiLookup/ID", "TestMultiLookup/Title", "TestMultiUser/ID", "TestMultiUser/Title", "TestUser/ID", "TestUser/Title"],
+                    Select: ["TestBoolean", "TestChoice", "TestDate", "TestLookup", "TestUrl", "Title"],
                     Top: 50
                 })
                 // Execute the request
@@ -86,69 +84,12 @@ export class DataSource {
                     // Ensure the items exist
                     if (items.results) {
                         // Resolve the request
-                        resolve(itemId > 0 ? items.results[0] : items.results as any);
+                        resolve(items.results);
                     } else {
                         // Reject the request
                         reject();
                     }
                 });
-        });
-    }
-
-    // Method to save a test item
-    save = (item: ITestItem): PromiseLike<ITestItem> => {
-        // Return a promise
-        return new Promise((resolve, reject) => {
-            // See if this is an existing item
-            if (item.update) {
-                // Update the item
-                item.update({
-                    TestBoolean: item.TestBoolean,
-                    TestChoice: item.TestChoice,
-                    TestDate: item.TestDate,
-                    TestDateTime: item.TestDateTime,
-                    TestLookupId: item.TestLookupId,
-                    TestMultiChoice: item.TestMultiChoice,
-                    TestMultiLookupId: item.TestMultiLookupId,
-                    TestMultiUserId: item.TestMultiUserId,
-                    TestNote: item.TestNote,
-                    TestNumberDecimal: item.TestNumberDecimal,
-                    TestNumberInteger: item.TestNumberInteger,
-                    TestUrl: item.TestUrl,
-                    TestUserId: item.TestUserId
-                } as ITestItem)
-                    // Execute the request
-                    .execute((request) => {
-                        // Ensure the update was successful
-                        if (request.response == "") {
-                            // Resolve the request
-                            resolve(item);
-                        } else {
-                            // Reject the request
-                            reject(request.response);
-                        }
-                    });
-            } else {
-                // Set the item metadata - This is required for complex field updates
-                item["__metadata"] = { type: this._listItemEntityTypeFullName };
-
-                // Get the web
-                (new Web(this._cfg.WebUrl))
-                    // Get the list
-                    .Lists(this._cfg.ListName)
-                    // Get the items
-                    .Items()
-                    // Add the item
-                    .add(item)
-                    // Execute the request
-                    .execute((item: ITestItem) => {
-                        // Load the item again to get the expanded field values
-                        this.load(item.Id).then((item: ITestItem) => {
-                            // Resolve the request
-                            resolve(item);
-                        });
-                    });
-            }
         });
     }
 }

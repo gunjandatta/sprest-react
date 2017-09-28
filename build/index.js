beeaf0f07b1f389e9168687693fe8e2e95851277
Gunjan Datta <me@dattabase.com>
2017-09-27 23:12:50 -0400

    Fixed reference for base field.

--- build/index.js
+++ build/index.js
@@ -3,10 +3,10 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
+var Fields = require("./fields");
+exports.Fields = Fields;
 __export(require("./common"));
 __export(require("./components"));
 __export(require("./definitions"));
-var Fields = require("./fields");
-exports.Fields = Fields;
 __export(require("./webparts"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import * as Fields from "./fields";
+
 export * from "./common";
 export * from "./components";
 export * from "./definitions";
-import * as Fields from "./fields";
 export { Fields };
 export * from "./webparts";

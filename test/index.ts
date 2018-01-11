6d5accfe10ae5fa33136e7ccac3d743e6918c0b1
Gunjan Datta <me@dattabase.com>
2018-01-11 18:59:51 -0500

    Removed unneeded imports.

--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,3 @@
-import "core-js/es6/map";
-import "core-js/es6/promise";
-import "core-js/es6/set";
 import { initializeIcons } from "@uifabric/icons";
 import { FabricWebPart, WebPartSearchCfg, WebPartTabs } from "../src";
 import { Configuration } from "./cfg";
